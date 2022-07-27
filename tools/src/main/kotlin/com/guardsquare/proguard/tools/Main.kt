321cb21c5892402e0dbc65d0c546dfad031e09c3
Pramitha Fernando <pramitha.fernando@guardsquare.com>
2022-07-27 13:37:01 +0200

    add printing methods

--- tools/src/main/kotlin/com/guardsquare/proguard/tools/Main.kt
+++ tools/src/main/kotlin/com/guardsquare/proguard/tools/Main.kt
@@ -1,28 +1,50 @@
 package com.guardsquare.proguard.tools
 
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ExperimentalCli
-import kotlinx.cli.Subcommand
-import proguard.classfile.ClassPool
+import dexreader.reader.DexClassReader
+import kotlinx.cli.*
+import proguard.classfile.*
+import proguard.classfile.visitor.AllMethodVisitor
 import proguard.classfile.visitor.ClassNameFilter
 import proguard.classfile.visitor.ClassPoolFiller
+import proguard.classfile.visitor.MemberVisitor
 import proguard.io.*
 import proguard.util.ExtensionMatcher
 import proguard.util.OrMatcher
 import java.io.File
-import java.io.IOException
 
 val parser = ArgParser("proguard-core-tools")
 @ExperimentalCli
 fun main(args: Array<String>) {
 
     class Dex2Jar : Subcommand("dex2jar", "Convert dex to jar") {
+
+        var input by argument(ArgType.String, description = "Input file name")
+        var output by option(ArgType.String, description = "Output file name").default("dex2jar_output.jar")
+
         override fun execute() {
-            TODO("Not yet implemented")
+
+            val programClassPool = readJar(input, "**", false)
+
+            programClassPool?.classesAccept(AllMethodVisitor(MyMethodPrinter()))
+
+        }
+
+        inner class MyMethodPrinter : MemberVisitor {
+            override fun visitAnyMember(clazz: Clazz, member: Member) {}
+            override fun visitProgramMethod(programClass: ProgramClass, programMethod: ProgramMethod) {
+                println(
+                    programClass.name + "." +
+                            programMethod.getName(programClass) +
+                            programMethod.getDescriptor(programClass)
+                )
+            }
         }
     }
 
     class Jar2Dex : Subcommand("jar2dex", "Convert jar to dex") {
+
+        var input by argument(ArgType.String, description = "Input file name")
+        var output by option(ArgType.String, description = "Output file name")
         override fun execute() {
             TODO("Not yet implemented")
         }
@@ -49,13 +71,16 @@ fun readJar(
     val source: DataEntrySource = FileSource(
         File(jarFileName)
     )
+
+    val acceptedClasssVisitor = ClassPoolFiller(classPool)
+
     var classReader: DataEntryReader = NameFilteredDataEntryReader(
         "**.class",
         ClassReader(
             isLibrary, false, false, false, null,
             ClassNameFilter(
                 classNameFilter,
-                ClassPoolFiller(classPool)
+                acceptedClasssVisitor
             )
         )
     )
@@ -63,14 +88,13 @@ fun readJar(
     // Convert dex files to a JAR first.
     classReader = NameFilteredDataEntryReader(
         "classes*.dex",
-        Dex2JarReader(
+        DexClassReader(
             !isLibrary,
-            classReader
+            acceptedClasssVisitor
         ),
-        classReader
-    )
+        classReader)
 
-//         Extract files from an archive if necessary.
+    // Extract files from an archive if necessary.
     classReader = FilteredDataEntryReader(
         DataEntryNameFilter(ExtensionMatcher("aar")),
         JarReader(
