ff6ef58435ad1d9b0e75e0928f7dad5de26dad41
Pramitha Fernando <pramitha.fernando@guardsquare.com>
2022-07-27 14:29:03 +0200

    add subcommands dex2jar & dex-printer

--- tools/src/main/kotlin/com/guardsquare/proguard/tools/Main.kt
+++ tools/src/main/kotlin/com/guardsquare/proguard/tools/Main.kt
@@ -3,44 +3,84 @@ package com.guardsquare.proguard.tools
 import dexreader.reader.DexClassReader
 import kotlinx.cli.*
 import proguard.classfile.*
-import proguard.classfile.visitor.AllMethodVisitor
-import proguard.classfile.visitor.ClassNameFilter
-import proguard.classfile.visitor.ClassPoolFiller
-import proguard.classfile.visitor.MemberVisitor
+import proguard.classfile.visitor.*
 import proguard.io.*
 import proguard.util.ExtensionMatcher
 import proguard.util.OrMatcher
+import java.io.Closeable
 import java.io.File
+import java.io.OutputStream
 
 val parser = ArgParser("proguard-core-tools")
 @ExperimentalCli
 fun main(args: Array<String>) {
 
-    class Dex2Jar : Subcommand("dex2jar", "Convert dex to jar") {
+    class DexPrinter : Subcommand("dumpclasses", "Dump all the classes") {
 
         var input by argument(ArgType.String, description = "Input file name")
-        var output by option(ArgType.String, description = "Output file name").default("dex2jar_output.jar")
 
         override fun execute() {
 
             val programClassPool = readJar(input, "**", false)
 
-            programClassPool?.classesAccept(AllMethodVisitor(MyMethodPrinter()))
+            programClassPool?.classesAccept(ClassPrinter())
 
         }
+//        inner class MyMethodPrinter : MemberVisitor {
+//            override fun visitAnyMember(clazz: Clazz, member: Member) {}
+//            override fun visitProgramMethod(programClass: ProgramClass, programMethod: ProgramMethod) {
+//                println(
+//                    programClass.name + "." +
+//                            programMethod.getName(programClass) +
+//                            programMethod.getDescriptor(programClass)
+//                )
+//            }
+//        }
 
-        inner class MyMethodPrinter : MemberVisitor {
-            override fun visitAnyMember(clazz: Clazz, member: Member) {}
-            override fun visitProgramMethod(programClass: ProgramClass, programMethod: ProgramMethod) {
-                println(
-                    programClass.name + "." +
-                            programMethod.getName(programClass) +
-                            programMethod.getDescriptor(programClass)
+    }
+
+    class Dex2Jar : Subcommand("dex2jar", "Convert dex to jar") {
+
+        var input by argument(ArgType.String, description = "Input file name")
+        var output by option(ArgType.String, description = "Output file name", shortName = "o").default("dex2jar_output.jar")
+
+        override fun execute() {
+            val programClassPool = readJar(input, "**", false)
+            writeJar(programClassPool!!, (File(output)), "Main")
+        }
+        private fun writeJar(programClassPool: ClassPool, file: File, mainClass: String) {
+            class MyJarWriter(zipEntryWriter: DataEntryWriter) : JarWriter(zipEntryWriter), Closeable {
+                override fun createManifestOutputStream(manifestEntry: DataEntry): OutputStream {
+                    val outputStream = super.createManifestOutputStream(manifestEntry)
+                    outputStream.writer().apply {
+                        appendLine("Main-Class: $mainClass")
+                        flush()
+                    }
+                    return outputStream
+                }
+
+                override fun close() {
+                    super.close()
+                }
+            }
+
+            val jarWriter = MyJarWriter(
+                ZipWriter(
+                    FixedFileWriter(
+                        file
+                    )
+                )
+            )
+
+            jarWriter.use {
+                programClassPool.classesAccept(
+                    DataEntryClassWriter(it)
                 )
             }
         }
     }
 
+    // TODO
     class Jar2Dex : Subcommand("jar2dex", "Convert jar to dex") {
 
         var input by argument(ArgType.String, description = "Input file name")
@@ -52,7 +92,9 @@ fun main(args: Array<String>) {
 
     val dex2jar = Dex2Jar()
     val jar2Dex = Jar2Dex()
-    parser.subcommands(dex2jar, jar2Dex)
+    val dumpclaases = DexPrinter()
+
+    parser.subcommands(dex2jar, jar2Dex, dumpclaases)
     parser.parse(args)
 
 }
